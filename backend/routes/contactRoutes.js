a6bda9de80427f6eea474b0a94b4c82a3889e868
Sp Ramanathan <subburamanathan7@gmail.com>
2023-11-11 06:48:58 +0530

    Route Update

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const {listContacts, addContact, updateContact, deleteContact, globalList, getContact, transferContact, fileUpload} = require('../controllers/contactController')
 const {authenticateVolunteer} = require('../middlewares/authMiddleware')
 
-router.route('/api/database/').get(authenticateVolunteer,listContacts).post(authenticateVolunteer,addContact)
-router.route('/api/database/:id').get(authenticateVolunteer,getContact).put(authenticateVolunteer,updateContact).delete(authenticateVolunteer,deleteContact)
+router.route('/').get(authenticateVolunteer,listContacts).post(authenticateVolunteer,addContact)
+router.route('/:id').get(authenticateVolunteer,getContact).put(authenticateVolunteer,updateContact).delete(authenticateVolunteer,deleteContact)
 
-router.route('/api/database/upload').post(authenticateVolunteer,fileUpload)
-router.route('/api/database/list/globalHR').get(authenticateVolunteer,globalList)
-router.route('/api/database/transfer/:id').put(authenticateVolunteer,transferContact)
+router.route('/upload').post(authenticateVolunteer,fileUpload)
+router.route('/list/globalHR').get(authenticateVolunteer,globalList)
+router.route('/transfer/:id').put(authenticateVolunteer,transferContact)
 
-
+ 
 module.exports = router;
--- backend/routes/volunteerRoutes.js
+++ backend/routes/volunteerRoutes.js
@@ -5,11 +5,11 @@ const {authenticateVolunteer} = require('../middlewares/authMiddleware')
 
 const {registerVolunteer, loginVolunteer, getMe, getDirectors, getUsers} = require('../controllers/volunteerController');
 
-router.route('/api/volunteer/register').post(authenticateVolunteer,registerVolunteer)
-router.route('/api/volunteer/login').post(loginVolunteer)
+router.route('/register').post(authenticateVolunteer,registerVolunteer)
+router.route('/login').post(loginVolunteer)
 
-router.route('/api/volunteer/profile').get(authenticateVolunteer,getMe)
-router.route('/api/volunteer/directors').get(authenticateVolunteer,getDirectors)
-router.route('/api/volunteer/users').get(authenticateVolunteer,getUsers)
+router.route('/profile').get(authenticateVolunteer,getMe)
+router.route('/directors').get(authenticateVolunteer,getDirectors)
+router.route('/users').get(authenticateVolunteer,getUsers)
 
 module.exports = router;
